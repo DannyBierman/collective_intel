983c67afa5c47d82a26e16beba2693c83cf019d3
DannyBierman <122381069+DannyBierman@users.noreply.github.com>
2023-06-15 16:58:47 +0200

    working

--- ass1/from enum import Enum, auto.py
+++ ass1/from enum import Enum, auto.py
@@ -1,12 +1,13 @@
 from enum import Enum, auto
 from turtle import pos
-from vi import Agent, Simulation
+from vi import Agent, Simulation, probability
 import pygame as pg
 from pygame.math import Vector2
 from vi import Agent, Simulation
 from vi.config import Config, dataclass, deserialize
 import random
 import numpy as np
+import math
 
 
 
@@ -19,109 +20,57 @@ class Cockroach(Agent):
  state = "wandering"
  def wandering(self):
     self.continue_movement()
-    #if prob joining is true: join
-    self.state = "joining"
-    return self.pos
+    if self.probability_join():
+        self.state = "joining"
 
- def probability_join(self, Neighbours):
-    neighbour_count = 0
-    for bird, distance in Neighbours:
-        neighbour_count +=1
 
-    probability = 0
-    if neighbour_count > 5:
-        probability = 1
-    else :
-        probability = neighbour_count / 5
-        
-    if probability > self.probability_threshold:
-            return True
-        
-    elif probability == self.probability_threshold:
-        probability = np.random.randint(0, 1)
-        if probability == 1:
-            return True 
-        else :
-            return False
-    
-    elif probability < self.probability_threshold:
-            return False
 
- def joining(self, Neighbours):
+ def joining(self, neighbours):
     neighbours = list(self.in_proximity_accuracy())
-    bird_positions = [bird.pos for bird, distance in neighbours]
-    position_sum = sum(bird_positions, Vector2())
-    if self.probability_join(neighbours):
-        average_pos = position_sum / len(neighbours)
-        force_c = average_pos - self.pos
+
+    if len(neighbours):
         self.state = "still"
-        return force_c - self.move
-    else:
-        return Vector2()
- # def probability_leave(self, Neighbours):
- #
- #    neighbour_count = 0
- #    for bird, distance in Neighbours:
- #        neighbour_count +=1
- #
- #    probability = 0
- #    if neighbour_count > 5:
- #        probability = 1
- #    else :
- #        probability = neighbour_count / 5
- #
- #    if probability < CockroachConfig.probability_threshold:
- #        return True
- #
- #    elif probability == CockroachConfig.probability_threshold:
- #        probability = np.random.randint(0, 1)
- #        if probability == 1:
- #            return True
- #        else :
- #            return False
- #
- #    elif probability > CockroachConfig.probability_threshold:
- #        return False
- #
 
+ def probability_join(self) -> bool:
+     return probability(1.15 ** (self.in_proximity_accuracy().count() + 1) - 1.15)
 
+ def probability_leave(self, Neighbours):
+     return probability(0.1 / (1 + math.exp(0.5 * self.in_proximity_accuracy().count())))
 
     
  def leaving(self, Neighbours):
     neighbours = list(self.in_proximity_accuracy())
-    bird_positions = [bird.pos for bird, distance in neighbours]
-    position_sum = sum(bird_positions, Vector2())
-    if random.random() < self.probability_leave(neighbours):
-        average_pos = position_sum / len(neighbours)
-        force_c = self.pos - average_pos
-        return force_c - self.move
-    else:
-        #stay
-        return Vector2()
-            
- def still(self):
-    if self.counter == 80:
-        self.freeze_movement()
 
- # def update(self):
- #    if self.on_site():
- #        self.counter += 1
- #        self.still()
- #    elif self.wandering :
- #        self.continue_movement()
- #    elif self.wandering and self.probability_join:
- #        self.joining
+    if self.counter % 110 == 0:
+
+        self.continue_movement()
+        self.state = "wandering"
+
+ def still(self):
+    if self.counter % 40 == 0 and self.on_site():
+        self.freeze_movement()
+        self.state = "leaving"
+
+
  def update(self):
     if self.state == "wandering":
-
+        print("state wandering")
         self.wandering()
     elif self.state == "joining":
+        print("state joining")
+
         self.joining(self.in_proximity_accuracy())
+
     elif self.state == "still":
+        print("state still")
+
         self.counter += 1
-        self.still
+        self.still()
     elif self.state == "leaving":
-        self.leave(self.in_proximity_accuracy())
+        print("state leaving")
+        self.counter += 1
+
+        self.leaving(self.in_proximity_accuracy())
 
 
 
@@ -132,7 +81,7 @@ data_frame = (
     Simulation(
         Config(
             image_rotation=True,
-            movement_speed=1,
+            movement_speed=10,
 
             radius=100,
             seed=1,
