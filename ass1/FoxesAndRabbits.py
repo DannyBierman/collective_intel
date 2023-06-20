3305b8bfc044fa42b7c539d417533666449cb34b
DannyBierman <122381069+DannyBierman@users.noreply.github.com>
2023-06-20 18:45:05 +0200

    grass

--- ass1/FoxesAndRabbits.py
+++ ass1/FoxesAndRabbits.py
@@ -4,11 +4,20 @@ from vi import Agent, Simulation, probability
 import pygame as pg
 from pygame.math import Vector2
 from vi import Agent, Simulation
-from vi.config import Config, dataclass, deserialize
+from vi.config import Config, dataclass, deserialize, Window
 import random
 import numpy as np
 import math
 import polars as pl
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class Configure():
+    Fox_population_counter= 20
+    Rabbit_population_counter = 50
+    Fox_population = []
+    Rabbit_population = []
+    pass
 
 class Foxes(Agent):
     energy = 1
@@ -20,7 +29,7 @@ class Foxes(Agent):
         position_sum = sum(target_rabbits, Vector2())
         if self.in_proximity_accuracy().filter_kind(Rabbit).count() > 0:
             average_pos = position_sum / self.in_proximity_accuracy().filter_kind(Rabbit).count()
-            self.move = self.pos-average_pos
+            self.move = (self.pos-average_pos)
             self.move = self.move.normalize()
             self.pos += self.move
 
@@ -31,40 +40,103 @@ class Foxes(Agent):
                 self.energy += 0.5
                 self.reproduce()
                 print("killed a rabbit")
+                Configure.Fox_population_counter += 1
+                Configure.Rabbit_population_counter -= 1
 
     def update(self):
         self.energy -= 0.01  # FOX_ENERGY_DECAY_RATE
         if self.energy <= 0:
             self.kill()
-        if self.in_proximity_accuracy():
-            self.chase_rabbit()
+            Configure.Fox_population_counter -= 1
+        if self.in_proximity_accuracy().filter_kind(Rabbit):
             self.kill_rabbit()
+        Configure.Fox_population.append(Configure.Fox_population_counter)
+        Configure.Rabbit_population.append(Configure.Rabbit_population_counter)
+            #self.chase_rabbit()
+
 
 class Rabbit(Agent):
-
+    energy = 1
+    counter = 0
     def reproducing(self):
         if random.random() < 0.004:
             self.reproduce()
+            Configure.Rabbit_population_counter += 1
+
+    def eating_grass(self):
+        for agent, distance in self.in_proximity_accuracy().filter_kind(Grass):
+            if distance < 10 and self.in_proximity_accuracy().filter_kind(Rabbit).count() == 0 and agent._image_index == 0:
+                self.freeze_movement()
+                self.counter += 1
+                if self.counter % 50 == 0:
+                    agent.change_image(1)
+                    agent.timer = 1
+                    self.energy += 0.5
+                    print("Ate grass")
+                    self.continue_movement()
+    def running(self):
+        foxes = []
+        for agent in self.in_proximity_accuracy().without_distance().filter_kind(Foxes):
+            foxes.append(agent.pos)
+        position_sum = sum(foxes, Vector2())
+        if self.in_proximity_accuracy().filter_kind(Foxes).count() > 0:
+            average_pos = position_sum / self.in_proximity_accuracy().filter_kind(Foxes).count()
+            self.move = self.pos-average_pos
+            self.move = self.move.normalize()
+            self.pos -= self.move
 
     def update(self):
-        self.reproducing()
+        self.energy -= 0.001
+        if not self.eating_grass():
+            self.reproducing()
+        self.eating_grass()
+        #if self.in_proximity_accuracy().without_distance().filter_kind(Foxes):
+            #self.running()
 
+class Grass(Agent):
+    timer = 0
+
+    def change_position(self):
+        pass
+
+    def update(self):
+        self.timer += 1
+        if self._image_index == 1 and self.timer % 180 == 0:
+            self.reproduce()
 
 
 data_frame = (
     Simulation(
         Config(
                 image_rotation=True,
-                movement_speed=10,
+                movement_speed=8,
                 # duration= 1*60,
                 radius=15,
                 seed=1,
                 fps_limit=30
+
         )
     )
         .batch_spawn_agents(50, Rabbit, images=["images/cockroach.png"])
         .batch_spawn_agents(20, Foxes, images=["images/bird.png"])
-
+        .batch_spawn_agents(20, Grass, images=["images/green2.png","images/green.png"])
         .run()
         .snapshots
+
 )
+indices = range(0, len(Configure.Fox_population), 100)
+
+
+population_subset = [Configure.Fox_population[i] for i in indices]
+
+plt.plot( indices, population_subset)
+
+r_indices = range(0, len(Configure.Rabbit_population), 100)
+r_population_subset = [Configure.Rabbit_population[i] for i in r_indices]
+
+plt.plot(r_indices, r_population_subset)
+
+plt.xlabel('Time')
+plt.ylabel('Fox Population')
+plt.title('Fox Population Over Time')
+plt.show()
